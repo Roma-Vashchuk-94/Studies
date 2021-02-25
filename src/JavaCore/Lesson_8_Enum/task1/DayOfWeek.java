84e944b6fa76ff643994ebc12eb270e4e8b7be62
Roman Vashchuk <Romeovaschuk94@gmail.com>
2021-02-25 20:04:45 +0200

    New commit. Enum, Exeptions

---
+++ src/JavaCore/Lesson_8_Enum/task1/DayOfWeek.java
@@ -0,0 +1,27 @@
+package JavaCore.Lesson_8_Enum.task1;
+
+public enum DayOfWeek {
+    SUNDAY("Sunday"),
+    MONDAY("Monday"),
+    TUESDAY("Tuesday"),
+    WEDNESDAY("Wednesday"),
+    THURSDAY("Thursday"),
+    FRIDAY("Friday"),
+    SATURDAY("Saturday");
+
+    private String title;
+
+    DayOfWeek(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    @Override
+    public String toString() {
+        return getTitle();
+    }
+}
