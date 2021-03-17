40b9c48bb53779b1e60dcf6fab7363036619b6ff
Roman Vashchuk <Romeovaschuk94@gmail.com>
2021-03-17 12:27:00 +0200

    New commit. Map(task_3), Reflection API.

---
+++ src/JavaCore/Lesson_15_Map/task3/Human.java
@@ -0,0 +1,50 @@
+package JavaCore.Lesson_15_Map.task3;
+
+import java.util.Objects;
+
+public class Human {
+
+    private String name;
+    private int age;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Human human = (Human) o;
+        return age == human.age &&
+                Objects.equals(name, human.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        String line = "person " + name + ", age - " + age;
+        return line;
+    }
+}
---
+++ src/JavaCore/Lesson_15_Map/task3/ZooClub.java
@@ -0,0 +1,97 @@
+package JavaCore.Lesson_15_Map.task3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ZooClub {
+    private char qMarks = '"';
+    private Map<Human, List<Animal>> zooClubList;
+
+    public ZooClub() {
+        zooClubList = new HashMap<>();
+    }
+
+    public Map<Human, List<Animal>> getZooClubList() {
+        return zooClubList;
+    }
+
+    public void setZooClubList(Map<Human, List<Animal>> zooClubList) {
+        this.zooClubList = zooClubList;
+    }
+
+    public void addHuman() {
+        System.out.println("\nYou chose " + qMarks + "Add a person" + qMarks +
+                "\nEnter name:");
+        String name = ZooClubTest.scanner.nextLine();
+        System.out.println("Enter age:");
+        int age = Integer.parseInt(ZooClubTest.scanner.nextLine());
+        Human human = new Human(name, age);
+        zooClubList.put(human, new ArrayList<>());
+        System.out.println("You added " + human + " to zoo club.");
+    }
+
+    public void addAnimal() {
+        System.out.println("\nYou chose " + qMarks + "Add the animal to a person" + qMarks +
+                "\nEnter person name:");
+        String name = ZooClubTest.scanner.nextLine();
+        System.out.println("Enter person age:");
+        int age = Integer.parseInt(ZooClubTest.scanner.nextLine());
+        Human human = new Human(name, age);
+        Animal animal = Animal.createAnimal();
+        if (isAnimalFree(animal)) {
+            zooClubList.get(human).add(animal);
+            System.out.println("You added " + animal + " to " + human);
+        }
+    }
+
+    public void removeAnimal() {
+        System.out.println("\nYou chose " + qMarks + "Remove the animal from a person" + qMarks +
+                "\nEnter person name:");
+        String name = ZooClubTest.scanner.nextLine();
+        System.out.println("Enter person age:");
+        int age = Integer.parseInt(ZooClubTest.scanner.nextLine());
+        Human human = new Human(name, age);
+        System.out.println("Enter animal type:");
+        Pets type = Pets.valueOf(ZooClubTest.scanner.nextLine().toUpperCase());
+        System.out.println("Enter animal name:");
+        String animalName = ZooClubTest.scanner.nextLine();
+        Animal animal = new Animal(type, animalName);
+        zooClubList.get(human).remove(animal);
+        System.out.println("You removed " + animal + " from a " + human);
+    }
+
+    public void removeHuman() {
+        System.out.println("\nYou chose " + qMarks + "Remove a person" + qMarks +
+                "\nEnter person name:");
+        String name = ZooClubTest.scanner.nextLine();
+        System.out.println("Enter person age:");
+        int age = Integer.parseInt(ZooClubTest.scanner.nextLine());
+        Human human = new Human(name, age);
+        zooClubList.remove(human);
+        System.out.println("You removed " + human + "from zoo club");
+    }
+
+
+
+    public boolean isAnimalFree(Animal animal) {
+        boolean isAnimalFree = true;
+        for (Map.Entry<Human, List<Animal>> entry : zooClubList.entrySet()) {
+            if (entry.getValue().contains(animal)) {
+                isAnimalFree = false;
+                System.out.println("Sorry. " + entry.getKey() + " has this animal (" + animal + ")");
+            }
+        }
+        return isAnimalFree;
+    }
+
+    public void print() {
+        int i = 1;
+        for (Map.Entry<Human, List<Animal>> entry : zooClubList.entrySet()) {
+            System.out.println("\nParticipant number " + i + ":\n" + entry.getKey() +
+                    "\nAnimals: " + entry.getValue());
+            i++;
+        }
+    }
+}
